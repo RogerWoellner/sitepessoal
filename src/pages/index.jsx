16da72dbc7c72ef18d2d6c54b443e5e9e9e0399a
RogerWoellner <115326179+RogerWoellner@users.noreply.github.com>
2023-03-19 01:04:26 -0300

    Add files via upload

---
+++ src/pages/index.jsx
@@ -0,0 +1,26 @@
+import { FaArrowRight } from 'react-icons/fa'
+import { Container, Content, Infos, Name, Function, Intro, LinkProjects, Logo, Img } from '../../styles/indexStyles'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+   <Container>
+     <Content>
+      <Infos>
+        <Name>Olá, sou Roger Woellner</Name>
+        <Function>FullStack Developer</Function>
+        <Intro>Estou no 3° semestre em Análise e Desenvolvimento, 
+          tenho experiência em desenvolvimento Web
+          (React, PHP, JS, HTML, CSS, Python, entre outros).
+        </Intro>
+        <Link href="projects">
+          <LinkProjects>PROJETOS <FaArrowRight /></LinkProjects>
+        </Link>
+      </Infos>
+      <Logo>
+        <Img src="/images/logo.png" alt="logo" />
+      </Logo>
+     </Content>
+   </Container>
+  )
+}
